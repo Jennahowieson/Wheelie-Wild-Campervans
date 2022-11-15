8cb1931e615cf97d1d0563e83d6d604120fee613
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-11-15 13:02:18 +0000

    added instances of vans, rentals and customers to database

--- db/wheeliewild.sql
+++ db/wheeliewild.sql
@@ -27,11 +27,36 @@ CREATE TABLE rentals(
     id SERIAL PRIMARY KEY
 );
 
-INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Jenna', 'True', 1000, 4);
-INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Kirsten', 'True', 1000, 3);
-INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Rae', 'False', 500, 4);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Jenna', 'True', 1000, 2);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Kirsten', 'True', 2000, 3);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Rae', 'True', 1500, 2);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Bruce', 'True', 1000, 1);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Connor', 'True', 500, 1);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Tina', 'False', 3000, 4);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Stewart', 'True', 1000, 3);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Fran', 'False', 750, 2);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Clare', 'True', 1000, 3);
+INSERT INTO customers (customer_name, license, budget, friends) VALUES ('Deb', 'False', 500, 1);
 
 
-INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('Rocky', 'SN12 743','2012', 5, 'Luxury');
-INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('Steve', 'SN13 624','2013', 3, 'Luxury');
-INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('Roadster', 'SR18 766','2018', 2, 'Budget');
+
+
+
+INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('Rocky', 'BA47 B04','2022', 5, 'Luxury');
+INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('VanGogh', 'VI1N C3T','2021', 4, 'Luxury');
+INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('Vanny DeVito', 'DA4N N0Y','2018', 3, 'Budget');
+INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('Van Halen', 'GU17 A4R','2019', 3, 'Budget');
+INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('RV There Yet', 'DR11 V1N','2017', 3, 'Luxury');
+INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('Home Sweet Roam', 'HH00 M3E','2016', 3, 'Luxury');
+INSERT INTO vans (van_name, reg_plate, year, capacity, type) VALUES ('Crewser', 'CR33 WSS','2020', 2, 'Budget');
+
+
+
+INSERT INTO rentals (customer_id, van_id, start_date, end_date) VALUES (1, 1, '2022-12-23', '2022-12-26');
+INSERT INTO rentals (customer_id, van_id, start_date, end_date) VALUES (2, 2, '2022-11-11', '2022-11-14');
+INSERT INTO rentals (customer_id, van_id, start_date, end_date) VALUES (3, 3, '2022-12-02', '2022-12-05');
+INSERT INTO rentals (customer_id, van_id, start_date, end_date) VALUES (4, 4, '2022-12-12', '2022-12-14');
+INSERT INTO rentals (customer_id, van_id, start_date, end_date) VALUES (5, 5, '2022-11-23', '2022-11-26');
+INSERT INTO rentals (customer_id, van_id, start_date, end_date) VALUES (6, 6, '2022-11-23', '2022-11-30');
+INSERT INTO rentals (customer_id, van_id, start_date, end_date) VALUES (7, 7, '2022-11-20', '2022-11-30');
+INSERT INTO rentals (customer_id, van_id, start_date, end_date) VALUES (8, 1, '2022-12-01', '2022-12-14');
